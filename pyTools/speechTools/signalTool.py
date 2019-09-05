e0348ceeab9cb6f78f80750123cffe2f3131aa7b
TonyWangX <wangxin@nii.ac.jp>
2019-09-05 18:34:29 +0900

    Add functions to speechTools/signalTool.py

--- pyTools/speechTools/signalTool.py
+++ pyTools/speechTools/signalTool.py
@@ -9,22 +9,40 @@ import scipy.io.wavfile
 from   ioTools import readwrite
 
 def spec(data, fft_bins=4096, frame_shift=40, frame_length=240):
+    """
+    f, t, cfft = scipy.signal.stft(data, nfft=4096, noverlap=frame_length-frame_shift, nperseg=frame_length)
+    """
     f, t, cfft = scipy.signal.stft(data, nfft=4096, noverlap=frame_length-frame_shift, nperseg=frame_length)
     return f,t,cfft
 
 def amplitude(cfft):
+    """
+    mag = amplitude(cfft)
+    return spectral amplitude given FFT data (cfft) in complex numbers
+    """
     mag = np.power(np.power(np.real(cfft),2) + np.power(np.imag(cfft),2), 0.5)
     return mag
 
 def amplitude_re_im(data_re, data_im):
+    """
+    mag = amplitude_re_im(data_re, data_im)
+    return spectral amplitude given real and imaginary part
+    """
     mag = np.power(data_re * data_re + data_im * data_im, 0.5)
     return mag
 
 def amplitude_to_db(mag):
+    """
+    20*np.log10(mag+ np.finfo(np.float32).eps)
+    """
     return 20*np.log10(mag+ np.finfo(np.float32).eps)
 
-def spec_amplitude(data):
-    _, _, cfft = spec(data)
+def spec_amplitude(data,fft_bins=4096, frame_shift=40, frame_length=240):
+    """
+    return spectral amplitude in db, given speech waveform data
+    mag_db = spec_amplitude(data,fft_bins=4096, frame_shift=40, frame_length=240):
+    """
+    _, _, cfft = spec(data, fft_bins, frame_shift, frame_length)
     mag  = amplitude(cfft)
     return amplitude_to_db(mag)
 
